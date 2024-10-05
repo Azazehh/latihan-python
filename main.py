e82f5e4da5b92cc0bb3d932e556c36a0ea972c38
Azazehh <rr660587@gmail.com>
2024-10-05 15:05:35 +0700

    add repository

---
+++ main.py
@@ -0,0 +1,35 @@
+# Mendefinisikan variabel
+nilai1 = 15
+nilai2 = 10
+nilai3 = 20
+
+# # operasi penjumlahan 
+# hasil_penjumlahan = angka1 + angka2 + angka3 
+# print("hasil penjumlahan:", hasil_penjumlahan)
+
+# # operasi pengurangan
+# hasil_pengurangan = angka1 - angka2 - angka3 
+# print("hasil pengurangan:", hasil_pengurangan)
+
+# # operasi perkalian
+# hasil_perkalian = angka1 * angka2 * angka3
+# print("hasil perkalian:", hasil_perkalian)
+
+# # operasi pembagian 
+# hasil_pembagian = angka1 / angka2 / angka3
+# print("hasil pembagian:", hasil_pembagian)
+
+# # operasi pemangkatan
+# hasil_pemangkatan = angka1 ** angka2 ** angka3
+# print("hasil pemangkatan:", hasil_pemangkatan)
+
+# print(angka1 == angka2)
+# print(angka1 != angka2)
+# print(angka1 > angka2)
+# print(angka1 < angka2)
+# print(angka1 >= angka2)
+# print(angka1 <= angka2)
+
+print(nilai2 == nilai3)
+print(nilai1 >= nilai3)
+print(nilai1 < nilai3)
